9a30979779609906e1429f5cfe21770db91c192f
Volodymyr Nerovnia <nerv@i.ua>
2024-12-24 15:26:59 +0200

    Complete main module

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,12 @@
-console.log('Simple typeScript template is ready!');
+import * as path from 'path';
+import { convertM3UtoCSV } from './converters/m3u';
+
+// Path
+const pathToRes = '@/../res/';
+
+const m3uFilePath = path.join(pathToRes, 'stations.m3u');
+const csvFilePath = path.join(pathToRes, 'stations.csv');
+
+convertM3UtoCSV(m3uFilePath, csvFilePath)
+    .then(() => console.log('Conversion complete!'))
+    .catch(err => console.error('Error converting M3U to CSV:', err));
